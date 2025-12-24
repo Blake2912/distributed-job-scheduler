0fc9e1e5eaed23c3cf2ff470fbff6592fe576dff
Varun K C <varun@Varuns-MacBook-Air.local>
2025-12-24 21:02:07 +0530

    Changes for integrating kubernetes into go

---
+++ distributed_job_scheduler_be/core/pod_library/pods/pods.go
@@ -0,0 +1,29 @@
+package pods
+
+import (
+	"context"
+
+	"example.com/pod_library/client"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func List(ctx context.Context, c *client.K8sClient, namespace string) ([]corev1.Pod, error) {
+	pods, err := c.Clientset.
+		CoreV1().
+		Pods(namespace).
+		List(ctx, metav1.ListOptions{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pods.Items, nil
+}
+
+func Create(ctx context.Context, c *client.K8sClient, namespace string, pod *corev1.Pod) (*corev1.Pod, error) {
+	return c.Clientset.
+		CoreV1().
+		Pods(namespace).
+		Create(ctx, pod, metav1.CreateOptions{})
+}
