967e0ca2a5a2cf079883b4ca4f3229cd41500582
Varun K C <kcvarun29122001@gmail.com>
2026-01-11 19:46:47 +0530

    Added loggers and insert batch size

--- scheduler-service/sql_dal/config/database.go
+++ scheduler-service/sql_dal/config/database.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -26,7 +28,20 @@ func ConnectDB(ctx context.Context) error {
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_SSLMODE"),
 	)
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "[GORM] ", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             300 * time.Millisecond,
+			LogLevel:                  logger.Warn,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  false,
+		},
+	)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		CreateBatchSize: 250,
+		Logger:          newLogger,
+	})
 
 	if err != nil {
 		return fmt.Errorf("Failed to connect to database: %w", err)
